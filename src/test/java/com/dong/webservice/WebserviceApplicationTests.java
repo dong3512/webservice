af106661217d6e53351852b34432b54b3ac4050d
dong3512 <dlehddls1015@naver.com>
2022-05-18 23:40:29 +0900

    Changes

--- src/test/java/com/dong/webservice/WebserviceApplicationTests.java
+++ src/test/java/com/dong/webservice/WebserviceApplicationTests.java
@@ -1,13 +1,32 @@
 package com.dong.webservice;
 
-import org.junit.jupiter.api.Test;
+import com.dong.webservice.web.HelloController;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @SpringBootTest
+@RunWith(SpringRunner.class)
+@WebMvcTest(controllers = HelloController.class)
 class WebserviceApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+	@Autowired
+	private MockMvc mvc;
 
+	@Test
+	public void hello가_리턴된다() throws Exception {
+		String hello = "hello";
+
+		mvc.perform(get("/hello"))
+				.andExpect(status().isOk())
+				.andExpect(content().string(hello));
+	}
 }
