fa558f450e9dd6bb2d63110c09c5360feed8092c
dong3512 <dlehddls1015@naver.com>
2022-05-26 23:58:01 +0900

    Changes

--- src/main/java/com/dong/webservice/service/posts/PostsServvice.java
+++ src/main/java/com/dong/webservice/service/posts/PostsServvice.java
@@ -1,4 +1,20 @@
 package com.dong.webservice.service.posts;
 
+import com.dong.webservice.domain.posts.PostsRepository;
+import com.dong.webservice.web.dto.PostsSaveRequestDto;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+
+@RequiredArgsConstructor
+@Service
 public class PostsService {
+    private final PostsRepository postsRepository;
+
+//    @Transactional
+//    public Long save(PostsSaveRequestDto requestDto) {
+//        return postsRepository.save(requestDto.toEntity())
+//                .getId();
+//    }
 }
