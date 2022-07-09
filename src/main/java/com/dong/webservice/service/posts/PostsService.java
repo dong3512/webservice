830379773a6b3bc85ef3539deae6ca87f2d95faa
dong3512 <dlehddls1015@naver.com>
2022-07-09 18:53:48 +0900

    7-09 -1

--- src/main/java/com/dong/webservice/service/posts/PostsService.java
+++ src/main/java/com/dong/webservice/service/posts/PostsService.java
@@ -2,12 +2,17 @@ package com.dong.webservice.service.posts;
 
 import com.dong.webservice.domain.posts.Posts;
 import com.dong.webservice.domain.posts.PostsRepository;
+import com.dong.webservice.web.dto.PostsListResponseDto;
 import com.dong.webservice.web.dto.PostsResponseDto;
 import com.dong.webservice.web.dto.PostsSaveRequestDto;
 import com.dong.webservice.web.dto.PostsUpdateRequestDto;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+
+import javax.servlet.http.PushBuilder;
+import java.util.List;
+import java.util.stream.Collectors;
 
 
 @RequiredArgsConstructor
@@ -33,5 +38,12 @@ public class PostsService {
         Posts entity = postsRepository.findById(id).orElseThrow(() ->
                 new IllegalArgumentException("해당 게시글이 없습니다. id=" + id));
         return new PostsResponseDto(entity);
+    }
+
+    @Transactional(readOnly = true)
+    public List<PostsListResponseDto> findAllDesc() {
+        return postsRepository.findAllDesc().stream()
+                .map(PostsListResponseDto::new)
+                .collect(Collectors.toList());
     }
 }
